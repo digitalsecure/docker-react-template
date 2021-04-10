f046cd6ebbbd919fba7542780d294958099cc7ba
digitalsecure <cool@outlook.sg>
2021-04-10 17:56:36 -0400

    Display images from robohash website

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 import './card.style.css';
 
-export const Card = (props) => {
-    return <div className='card-container'>
-        {
-           <h3 key={props}>{ props.monster.name }</h3>
-        }
+export const Card = (props) => (
+     <div className='card-container'>        
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?/set=set3&size=180x180`} />           
+        <h3 key={props}>{ props.monster.name }</h3>        
     </div>
-};
+);
