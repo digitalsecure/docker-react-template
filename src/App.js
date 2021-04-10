0765fe93fb68c0486e236c578e79d54132b6fe7e
digitalsecure <cool@outlook.sg>
2021-04-10 11:59:23 -0400

    Updated docker-compose with volumes on src and public folders. Update on App.js from function to class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>React Docker Template</h1>
-      This template provides the necessary Dockerfile to build a blank react project template.
-      
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string : 'Hello React Developer!!'
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>React Docker Template</h1>
+        This template provides the necessary Dockerfile to build a blank react project template.
+        <p>
+          { this.state.string }
+        </p>
+  
+      </div>
+    );
+  }
 }
+
+// function App() {
+  
+// }
 
 export default App;
