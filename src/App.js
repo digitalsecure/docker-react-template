a965fec34254c95d98bdf2cf1951f4db4a59223b
digitalsecure <cool@outlook.sg>
2021-04-10 21:33:47 -0400

    Added font for h1 tag and styling to css

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends Component {
     });
 
     return (
-      <div className="container">
-        <h1>React Docker Template</h1> 
+      <div className="App">
+        <h1>Monsters Rolodex</h1> 
         <SearchBox placeholder="search monsters" handleChange={ this.handleChange } />
         <p />
         <CardList monsters={ filteredMonsters } />
